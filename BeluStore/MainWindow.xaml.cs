f006d93e3b1cf7574b0fe8887f4af85fac1bb49b
Katorivn <78785812+katorivn699@users.noreply.github.com>
2024-10-08 19:12:49 +0700

    Add WPF fluent ui

--- BeluStore/MainWindow.xaml.cs
+++ BeluStore/MainWindow.xaml.cs
@@ -8,17 +8,20 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Wpf.Ui.Appearance;
 
 namespace BeluStore
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            ApplicationThemeManager.Apply(this);
         }
+
     }
 }
